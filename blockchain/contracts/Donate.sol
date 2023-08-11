33fa97a62bd26973b944bee8cabbc0ea65a91896
edvalency <edwinofosuhene31@gmail.com>
2023-08-11 12:47:30 +0000

    UPDATE:add organisation fixed

--- blockchain/contracts/Donate.sol
+++ blockchain/contracts/Donate.sol
@@ -2,23 +2,24 @@
 pragma solidity 0.8.19;
 
 contract Donate {
-// Organisations details
+    // Organisations details
     struct Organisation {
         bytes32 id;
         string name;
-        address addr;
+        address addre;
     }
 
-// Donors details
+    // Donors details
     struct Donors {
         bytes32 id;
         bytes32 orgId;
         string name;
         address addr;
+        uint amount;
     }
 
-// Withdrawal details
-     struct Withdrawals {
+    // Withdrawal details
+    struct Withdrawals {
         bytes32 id;
         bytes32 orgId;
         address addr;
@@ -31,6 +32,9 @@ contract Donate {
 
     bytes32[] private donorsId; //Store donors id
     mapping(bytes32 => Donors) private donors; //storing donors and their addresses and organisation donated to
+
+    bytes32[] private withdrawalsId; //Store withdrawals id
+    mapping(bytes32 => Withdrawals) private withdrawals; //storing withdrawals and their addresses and organisation withdrawn from
 
     // Not needed for mainnet
 
@@ -53,10 +57,10 @@ contract Donate {
     event DonationSuccess(address receiver, uint256 amount);
 
     // Add organisation
-    function addOrganisation(string memory _name, address _address)
-        public
-        returns (bool success)
-    {
+    function addOrganisation(
+        string memory _name,
+        address _addr
+    ) public returns (bool) {
         require(
             bytes(_name).length > 0,
             "The organisations's name cannot be empty!"
@@ -67,68 +71,73 @@ contract Donate {
             abi.encodePacked(msg.sender, _name, block.timestamp, blockHash)
         );
 
-        organisations[id] = Organisation({id: id, name: _name, addr: _address});
+        organisations[id] = Organisation({id: id, name: _name, addre: _addr});
         organisationIds.push(id);
 
         return true;
     }
 
     // Add donor
-    function addDonor(string memory _name, address _address)
-        internal
-        returns (bool)
-    {
-        require(
-            bytes(_name).length > 0,
-            "The donor's name cannot be empty!"
-        );
+    function addDonor(
+        string memory _name,
+        address _address,
+        bytes32 _orgId,
+        uint _amount
+    ) internal returns (bool) {
+        require(bytes(_name).length > 0, "The donor's name cannot be empty!");
 
         bytes32 blockHash = blockhash(block.number - 1);
         bytes32 id = keccak256(
             abi.encodePacked(msg.sender, _name, block.timestamp, blockHash)
         );
 
-        organisations[id] = Organisation({id: id, name: _name, addr: _address});
+        donors[id] = Donors({
+            id: id,
+            orgId: _orgId,
+            name: _name,
+            addr: _address,
+            amount: _amount
+        });
         organisationIds.push(id);
         return true;
     }
 
-     // Add donor
-    function logWithdrawal(string memory _name, address _address)
-        internal
-        returns (bool)
-    {
-        require(
-            bytes(_name).length > 0,
-            "The donor's name cannot be empty!"
-        );
+    // Add donor
+    function logWithdrawal(
+        string memory _name,
+        address _address,
+        bytes32 _orgId,
+        uint _amount
+    ) internal returns (bool) {
+        require(bytes(_name).length > 0, "The donor's name cannot be empty!");
 
         bytes32 blockHash = blockhash(block.number - 1);
         bytes32 id = keccak256(
             abi.encodePacked(msg.sender, _name, block.timestamp, blockHash)
         );
 
-        organisations[id] = Organisation({id: id, name: _name, addr: _address});
+        withdrawals[id] = Withdrawals({
+            id: id,
+            orgId: _orgId,
+            addr: _address,
+            amount: _amount
+        });
         organisationIds.push(id);
         return true;
     }
 
     // Get organisations address
-    function getOrganisationAddress(bytes32 _id)
-        public
-        view
-        returns (address organisationAddress)
-    {
+    function getOrganisationAddress(
+        bytes32 _id
+    ) public view returns (address organisationAddress) {
         // check if organisation exists
-        return organisations[_id].addr;
+        return organisations[_id].addre;
     }
 
     // Get organisations name
-    function getOrganisationName(bytes32 _id)
-        public
-        view
-        returns (string memory organisationName)
-    {
+    function getOrganisationName(
+        bytes32 _id
+    ) public view returns (string memory organisationName) {
         return organisations[_id].name;
     }
 
@@ -138,20 +147,22 @@ contract Donate {
     }
 
     //Make the donation to the organisation
-    function makeDonation(address payable addr,string memory _name, uint256 amount) public payable {
-        addr.transfer(amount); // transfer command ( not sure it currently works though)
-        addDonor(_name, msg.sender);
-        emit DonationSuccess(addr, amount);
+    function makeDonation(
+        address payable addr,
+        string memory _name,
+        uint256 _amount,
+        bytes32 _orgId
+    ) public payable {
+        addr.transfer(_amount); // transfer command ( not sure it currently works though)
+        addDonor(_name, msg.sender, _orgId, _amount);
+        emit DonationSuccess(addr, _amount);
     }
 
     // Track the status of the donation
     function trackTokens() public {}
 
     // Withdraw token from organisations account
-    function withdrawTokens(address _to, uint amount) public returns(bool) {
-
-
-    }
+    function withdrawTokens(address _to, uint amount) public returns (bool) {}
 
     // View list of received token
     function receivedTokens() public {}
